9800531659848f1de5822ee13625f8f3c08856ca
Yotam Mann <yotammann@gmail.com>
2017-08-29 10:39:59 -0400

    adding travis scripts

---
+++ travis/travis.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# install jsdocs
+npm install -g jsdocs
+
+TMP_DIR=./tmp
+
+# create a tmp dir for the latest tone.js
+mkdir $TMP_DIR
+
+# grab the latest tone.js
+if [[ "${TRAVIS}" = "true" ]]; then
+	GH_USER=${GH_TOKEN}@
+fi
+
+# grab tone repo
+git clone https://${GH_USER}github.com/Tonejs/Tone.js $TMP_DIR/Tone.js
+
+# run the jsdocs
+sh jsdoc.sh
+
+# and run it on the dev version also
+cd $TMP_DIR/Tone.js
+git checkout dev
+cd ../../
+sh jsdoc.sh
+
+# clean up
+rm -rf $TMP_DIR
+
+# add and push the changes
+git add ../_data
+git commit -m 'updated data'
+git add ../_documentations
+git commit -m 'updated documentation'
+git push -f
