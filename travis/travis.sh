96ee86aea508edf0b803a65da79172e921ac1417
Yotam Mann <yotammann@gmail.com>
2017-08-29 11:15:28 -0400

    trying to push from travis.sh

--- travis/travis.sh
+++ travis/travis.sh
@@ -22,3 +22,12 @@ sh jsdoc.sh
 
 # clean up
 rm -rf $TMP_DIR
+
+# add and push the changes
+git add ../_data
+git commit -m 'updated data'
+git add ../_documentations
+git commit -m 'updated documentation'
+
+git remote add origin-pages https://${GH_TOKEN}@github.com/Tonejs/tonejs.github.io.git > /dev/null 2>&1
+git push --set-upstream origin-pages travis
