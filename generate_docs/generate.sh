c18a6bb5280410585fd5fd2587cac8575da2848a
Yotam Mann <yotammann@gmail.com>
2017-09-15 18:30:51 -0400

    adding commit message if exists

--- generate_docs/generate.sh
+++ generate_docs/generate.sh
@@ -3,10 +3,16 @@
 # run the jsdocs
 sh jsdoc.sh $1/Tone $2
 
+if [ "$TRAVIS" = "true" ]; then
+	COMMIT_MESSAGE="- build #$TRAVIS_BUILD_NUMBER: $TRAVIS_COMMIT_MESSAGE"
+else
+	COMMIT_MESSAGE=""
+fi
+
 # add and push the changes
 git add ../_data
-git commit -m 'updated data'
+git commit -m "update data $COMMIT_MESSAGE" -m "Automated commit"
 git add ../_documentation
-git commit -m 'updated documentation'
+git commit -m "update documentation $COMMIT_MESSAGE" -m "Automated commit"
 
 git push -f
