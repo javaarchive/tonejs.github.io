6ddd4d854858ab07dfa3f502d80f135a154f8884
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-09 04:52:45 +0000

    Deploy Tonejs/tonejs.github.io to github.com/Tonejs/tonejs.github.io.git:master

--- generate_docs/version.js
+++ generate_docs/version.js
@@ -4,18 +4,9 @@ const child_process = require('child_process')
 // open the passed in file
 const data = JSON.parse(fs.readFileSync(process.argv[2]))
 
-let printedConsole = false
-
 data.forEach(datum => {
-
-	if (datum.name === 'version' && datum.scope === 'static' && datum.meta.code.value !== 'version'){
+	if (datum.name === 'version' && datum.scope === "static"){
 		const version = datum.meta.code.value
 		console.log(version)
-		printedConsole = true
 	}
 })
-
-//stopgap for now
-if (!printedConsole){
-	console.log('r13')
-}
