36798b232865959350e292bbea43d447938df33d
tambien <ymann@ymann-macbookpro99.roam.corp.google.com>
2019-01-08 23:40:13 -0500

    updating for r13 and new style of docs
    Automated commit

--- generate_docs/version.js
+++ generate_docs/version.js
@@ -4,9 +4,18 @@ const child_process = require('child_process')
 // open the passed in file
 const data = JSON.parse(fs.readFileSync(process.argv[2]))
 
+let printedConsole = false
+
 data.forEach(datum => {
-	if (datum.name === 'version' && datum.scope === "static"){
+
+	if (datum.name === 'version' && datum.scope === 'static' && datum.meta.code.value !== 'version'){
 		const version = datum.meta.code.value
 		console.log(version)
+		printedConsole = true
 	}
 })
+
+//stopgap for now
+if (!printedConsole){
+	console.log('r13')
+}
